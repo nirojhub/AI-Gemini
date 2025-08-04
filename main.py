95a0cc936992349182c8eb64f98f95a55c8f85b0
Niroj <nirojkumarsahoo55@gmail.com>
2025-08-04 12:00:31 +0530

    created the main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# pip install google-genai
+# This code snippet demonstrates how to use the Google GenAI client to generate content.
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key='AIzaSyA2G7UC7CX1ZHZBQoKJzk-iGStL6xcQsBQ')
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
+
+# Upload a file and generate content based on it
+'''file = client.files.upload(file='a11.txt')
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=['Could you summarize this file?', file]
+)
+print(response.text)'''
+
+# Example of generating content with specific configurations
+'''response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=types.Part.from_text(text='Why is the sky blue?'),
+    config=types.GenerateContentConfig(
+        temperature=0,
+        top_p=0.95,
+        top_k=20,
+        candidate_count=1,
+        seed=5,
+        max_output_tokens=100,
+        stop_sequences=['STOP!'],
+        presence_penalty=0.0,
+        frequency_penalty=0.0,
+    ),
+)
+
+print(response.text)'''
